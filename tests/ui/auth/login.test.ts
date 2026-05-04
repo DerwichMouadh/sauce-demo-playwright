3415557e94c2253e42e603debea9d779d7a8a4e0
Mouadh Derwich <dmouadh040@gmail.com>
2026-05-04 02:20:24 +0100

    refactor: introduce centralized test data for users

--- tests/ui/auth/login.test.ts
+++ tests/ui/auth/login.test.ts
@@ -1,8 +1,9 @@
 import { test, expect } from '@playwright/test';
 import { login } from '../../../helpers/authHelper';
+import { users } from '../../data/users';
 
 test('User can login with valid credentials', async ({ page }) => {
-  await login(page, 'standard_user', 'secret_sauce');
+  await login(page, users.standard.username, users.standard.password);
 
   await expect(page).toHaveURL(/inventory/);
 });
